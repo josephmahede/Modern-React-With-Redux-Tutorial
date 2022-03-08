b289b7b5460817c2d4c35f47d6fcccf95c4255c8
Joseph Ma <dev@mahedejoseph.com>
2022-03-08 05:35:14 -0800

    videos application finished. Section 11 finished.

---
+++ videos/src/components/VideoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import VideoItem from "./VideoItem";
+
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return (
+      <VideoItem
+        onVideoSelect={onVideoSelect}
+        video={video}
+        key={video.id.videoId}
+      />
+    );
+  });
+
+  return (
+    <div>
+      Found {videos.length} videos.
+      <div className="ui relaxed divided list">{renderedList}</div>
+    </div>
+  );
+};
+
+export default VideoList;
